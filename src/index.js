91f155b231c7c88cd0e4c518e472881972cb0ebb
megapolis123 <al.averin123@gmail.com>
2019-03-21 20:08:36 +0300

    feat: solved love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+
+  for (let i = 0; i < preferences.length; i++) {
+    let one = preferences[i] - 1;
+    let two = preferences[one] - 1;
+    let tree = preferences[two] - 1;
+
+    if (tree === i) {
+      count++;
+    }
+  }
+  return Math.floor(count / 3);
 };
